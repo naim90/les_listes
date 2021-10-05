41660aa029c87eb437fc7927cf8ed801c969d3ea
naim90 <naim.elmoudni@gmail.com>
2021-10-05 12:14:59 +0200

    modification pour accumuler les trois modifications

--- Program.cs
+++ Program.cs
@@ -18,47 +18,38 @@ namespace Liste
                 Console.WriteLine(affichageListe0);
             }
 
-            Console.WriteLine("\n1-Supprimer la première avec RemoveAt\n2-Supprimer la 3ème avec Remove\n3-Modifier la deuxième avec son indice :\n");
-            int choix = Convert.ToInt32(Console.ReadLine());
-            Program.Liste(stringCollection,choix);
+            Console.WriteLine("\n\n");
+
+            stringCollection.RemoveAt(0);
+
+            foreach (string affichageListe1 in stringCollection)
+            {
+                Console.WriteLine(affichageListe1);
+            }
+            Console.WriteLine("\n\n");
+
+            stringCollection.Remove("JP va dormir");
+
+            foreach (string affichageListe2 in stringCollection)
+            {
+                Console.WriteLine(affichageListe2);
+            }
+
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine("Entrer la phrase qui remplacera la ligne 2 de la liste :\n");
+            string phrase = Console.ReadLine();
+            stringCollection[0] = phrase;
+            Console.WriteLine("\n\n");
+
+            foreach (string affichageListe in stringCollection)
+            {
+                Console.WriteLine(affichageListe);
+            }
+
         }
 
-        public static void Liste(List<string> stringCollection,int a)
-        {
-
-
-            if (a == 1)
-            {
-                stringCollection.RemoveAt(0);
-
-                foreach (string affichageListe1 in stringCollection)
-                {
-                    Console.WriteLine(affichageListe1);
-                }
-            }
-
-            if (a == 2)
-            {
-                stringCollection.Remove("JP va dormir");
-
-                foreach (string affichageListe2 in stringCollection)
-                {
-                    Console.WriteLine(affichageListe2);
-                }
-            }
-
-            if (a == 3)
-            {
-                Console.WriteLine("Entrer la phrase qui remplacera la ligne 2 de la liste :\n");
-                string phrase=Console.ReadLine();
-                stringCollection[1] = phrase;
-
-                foreach (string affichageListe in stringCollection)
-                {
-                    Console.WriteLine(affichageListe);
-                }
-            }
 
         }
     }
-}
+
